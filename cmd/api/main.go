e91a767b9dd3a7323aaea2a315d5f07f57067896
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-29 01:52:51 +0500

    Создал функцию для подключения к Бд PostgreSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log/slog"
+	"os"
 
 	"github.com/shodruzhoshimzoda/tojtech/internal/config"
+	"github.com/shodruzhoshimzoda/tojtech/internal/repository/postgres"
 	"github.com/shodruzhoshimzoda/tojtech/pkg/logger"
 )
 
@@ -16,11 +19,23 @@ func main() {
 	logger := logger.SetupLogger(cfg.Env)
 
 	logger.Info("Logger initialized", slog.String("env", cfg.Env))
+
 	logger.Debug("DEBUG mode  enabled")
 
+	ctx := context.Background()
 
+	db, err  := postgres.ConnectionDB(ctx, cfg.GetDSN())
 
-	// TOOD: Initialize database connection
+	if err != nil {
+		logger.Error("unable connection to Database: ", slog.String("err",err.Error()))
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Info("Connection to database was successfully")
+
+	
 	// TOOD: Initialize repository layer
 	// TOOD: Initialize service (usecase) layer
 	// TOOD: Initialize HTTP server and routes
