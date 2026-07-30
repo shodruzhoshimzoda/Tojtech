35215aa2da538589ac4cd9a1543be9e4a9afc32f
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-31 00:01:35 +0500

    Was removed errors from here

--- internal/repository/postgres/product/product_repository.go
+++ internal/repository/postgres/product/product_repository.go
@@ -9,21 +9,11 @@ import (
 	domain "github.com/shodruzhoshimzoda/tojtech/internal/domain/product"
 )
 
-
-
-// 
-var (
-	ErrProductNotFound = errors.New("product not  found")
-)
-
-
+//
 
 type ProductRespository struct {
-	dbPool 	*pgxpool.Pool
+	dbPool *pgxpool.Pool
 }
-
-
-
 
 func NewProductRepository(dbPool *pgxpool.Pool) *ProductRespository {
 	return &ProductRespository{
@@ -31,31 +21,25 @@ func NewProductRepository(dbPool *pgxpool.Pool) *ProductRespository {
 	}
 }
 
+func (p *ProductRespository) GetById(ctx context.Context, id int64) (*domain.Product, error) {
 
+	query := "SELECT id, name, description, price from products where id=$1;"
 
-func (p *ProductRespository) GetById(ctx context.Context, id int64)  (*domain.Product, error) {
+	var product domain.Product
 
-
-		query := "SELECT id, name, description, price from products where id=$1;"
-
-
-		var product domain.Product
-
-		if err := p.dbPool.QueryRow(ctx, query, id).Scan(
-			&product.ID,
-			&product.Name,
-			&product.Description,
-			&product.Price,
-		); err != nil {
-			if errors.Is(err, pgx.ErrNoRows) {
-				return nil, ErrProductNotFound
-			}
-
-			return nil, err
+	if err := p.dbPool.QueryRow(ctx, query, id).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Price,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, domain.ErrProductNotFound
 		}
 
+		return nil, err
+	}
 
-		return &product, nil
-
+	return &product, nil
 
 }
