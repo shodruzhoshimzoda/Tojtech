80e7a981f8b950898958fc6707b22f6336b48db4
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-31 00:01:56 +0500

    Was removed validation for products for getting

--- internal/usecase/product/product_usecase.go
+++ internal/usecase/product/product_usecase.go
@@ -7,37 +7,27 @@ import (
 	domain "github.com/shodruzhoshimzoda/tojtech/internal/domain/product"
 )
 
-
 var (
 	ErrProductNotFound = errors.New("product not  found")
 )
 
-
-
-
-
 type ProductRepo interface {
-
-	GetById(ctx context.Context, id int64)	(*domain.Product, error)
+	GetById(ctx context.Context, id int64) (*domain.Product, error)
 }
 
-
-// Productusecase struct contain repository wcich means its doesnt matter which DB we use this 
-// Db must have this methods 
+// Productusecase struct contain repository wcich means its doesnt matter which DB we use this
+// Db must have this methods
 type ProductUsecase struct {
-	repo 	ProductRepo
+	repo ProductRepo
 }
-
-
 
 func NewProductUsecase(repo ProductRepo) *ProductUsecase {
-	return  &ProductUsecase{
+	return &ProductUsecase{
 		repo: repo,
 	}
 }
 
-
-// Bussines logic for GetProduct 
+// Bussines logic for GetProduct
 func (p *ProductUsecase) GetProduct(ctx context.Context, id int64) (*domain.Product, error) {
 
 	prod, err := p.repo.GetById(ctx, int64(id))
@@ -46,13 +36,6 @@ func (p *ProductUsecase) GetProduct(ctx context.Context, id int64) (*domain.Prod
 		return nil, err
 	}
 
-	err = prod.Validate()
-
-	if err != nil {
-		return nil, err
-	}
-
-	return prod,nil
-
+	return prod, nil
 
 }
