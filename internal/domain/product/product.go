595c73c65b12e4f0c21fb184a795895a20e65d13
sabrrise <shodruzxoshimzoda@gmail.com>
2026-08-05 21:31:20 +0500

    Add errors to this file

--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -1,10 +1,20 @@
 package domain_product
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
 	domain_category "github.com/shodruzhoshimzoda/tojtech/internal/domain/category"
+)
+
+var (
+	ErrInvalidProductName  = errors.New("The length of the product name must be less than 100 characters and greater than 0.")
+	ErrEmptyProductName    = errors.New("Product name could no be empty")
+	ErrInvalidProductPrice = errors.New("the product price is incorrect")
+	ErrLongDescription     = errors.New("The product description is too long")
+
+	ErrProductNotFound = errors.New("product not  found")
 )
 
 type Product struct {
