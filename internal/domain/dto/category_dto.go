c68eb58c261ba5e2f609816dcaeefc0347df0deb
shodruz <shodruzxoshimzoda@gmail.com>
2026-08-31 21:34:57 +0500

    Was created new DTO objects for transfering info

---
+++ internal/domain/dto/category_dto.go
@@ -0,0 +1,30 @@
+package dto
+
+import (
+	"time"
+	"uuid"
+
+	category_domain "github.com/shodruzhoshimzoda/tojtech/internal/domain/category"
+)
+
+
+type CategoryResponse struct {
+	UUID        uuid.UUID `json:"uuid"`
+	Name        string    `json:"name"`
+	Slug        string    `json:"slug"`
+	Description string    `json:"description"`
+	CreatedAt   string    `json:"created_at"`
+	UpdatedAt   string    `json:"updated_at"`
+}
+
+// NewCategoryResponse - DTO
+func NewCategoryResponse(c *category_domain.Category) CategoryResponse {
+	return CategoryResponse{
+		UUID:        uuid.UUID(c.UUID),
+		Name:        c.Name,
+		Slug:        c.Slug,
+		Description: c.Description,
+		CreatedAt:   c.CreatedAt.Format(time.DateTime),
+		UpdatedAt:   c.UpdatedAt.Format(time.DateTime),
+	}
+}
