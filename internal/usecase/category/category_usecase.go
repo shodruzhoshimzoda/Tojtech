1ece217e5ac8eb9a38278f5a130f816b4b404241
shodruz <shodruzxoshimzoda@gmail.com>
2026-08-31 21:35:35 +0500

    Was moved some business logic from handlers to usecases

--- internal/usecase/category/category_usecase.go
+++ internal/usecase/category/category_usecase.go
@@ -2,16 +2,19 @@ package usecase_category
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 
 	"github.com/google/uuid"
-	domain_category "github.com/shodruzhoshimzoda/tojtech/internal/domain/category"
+	category_domain "github.com/shodruzhoshimzoda/tojtech/internal/domain/category"
+	"github.com/shodruzhoshimzoda/tojtech/internal/domain/dto"
 )
 
 type CategoryRepo interface {
-	GetCategory(ctx context.Context, uuid uuid.UUID) (*domain_category.Category, error)
-	GetCategories(ctx context.Context) ([]*domain_category.Category, error)
-	CreateCategory(ctx context.Context, category *domain_category.Category) (uuid.UUID, error)
-	UpdateCategory(ctx context.Context, id uuid.UUID, category *domain_category.Category) (cat *domain_category.Category, e error)
+	GetCategory(ctx context.Context, uuid uuid.UUID) (*category_domain.Category, error)
+	GetCategories(ctx context.Context) ([]*category_domain.Category, error)
+	CreateCategory(ctx context.Context, category *category_domain.Category) (uuid.UUID, error)
+	UpdateCategory(ctx context.Context, id uuid.UUID, category *category_domain.Category) (cat *category_domain.Category, e error)
 	DeleteCategory(ctx context.Context, id uuid.UUID) (e error)
 }
 
@@ -25,19 +28,67 @@ func NewCategoryUseCase(categoryRepo CategoryRepo) *CategoryUseCase {
 	}
 }
 
-func (u *CategoryUseCase) GetCategory(ctx context.Context, uuid uuid.UUID) (*domain_category.Category, error) {
-	return u.categoryRepo.GetCategory(ctx, uuid)
+func (u *CategoryUseCase) GetCategory(ctx context.Context, rawUUID string) (dto.CategoryResponse, error) {
+	
+	id, err := uuid.Parse(rawUUID)
+	if err != nil {
+		return dto.CategoryResponse{}, category_domain.ErrInvalidUUID
+	}
+
+	category, err := u.categoryRepo.GetCategory(ctx, id)
+	if err != nil {
+		return dto.CategoryResponse{}, err
+	}
+
+	cat := dto.NewCategoryResponse(category)
+	return cat, nil
+
+	// return u.categoryRepo.GetCategory(ctx, uuid)
 }
 
-func (u *CategoryUseCase) CreateCategory(ctx context.Context, category *domain_category.Category) (uuid.UUID, error) {
+func (u *CategoryUseCase) CreateCategory(ctx context.Context, category *category_domain.Category) (dto.CategoryResponse, error) {
 
-	return u.categoryRepo.CreateCategory(ctx, category)
+	id, err :=  u.categoryRepo.CreateCategory(ctx, category)
 
+	if err != nil {
+		return dto.CategoryResponse{}, err
+	}
+	category.UUID = id
+	catDTO := dto.NewCategoryResponse(category)
+
+	return catDTO, nil
+	
 }
 
-func (u *CategoryUseCase) UpdateCategory(ctx context.Context, id uuid.UUID, category *domain_category.Category) (*domain_category.Category, error) {
+func (u *CategoryUseCase) UpdateCategory(r *http.Request,rawUUID string,) (dto.CategoryResponse, error) {
 
-	return u.categoryRepo.UpdateCategory(ctx, id, category)
+
+	id, err := uuid.Parse(rawUUID)
+
+	if err != nil {
+		return dto.CategoryResponse{}, category_domain.ErrInvalidUUID
+	}
+
+	var req category_domain.Category
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return	dto.CategoryResponse{}, category_domain.ErrInvalidRequestBody
+	}
+
+	if err = req.Validate(); err != nil {
+		return  dto.CategoryResponse{}, category_domain.ErrInvalidBody
+	}
+
+	newCategory, err := u.categoryRepo.UpdateCategory(r.Context(), id, &req)
+
+	if err != nil {
+		return dto.CategoryResponse{}, err
+	}
+
+	catDTO := dto.NewCategoryResponse(newCategory)
+
+	return catDTO, nil
+
+	// return u.categoryRepo.UpdateCategory(ctx, rawUUID, category)
 
 }
 
@@ -45,6 +96,24 @@ func (u *CategoryUseCase) DeleteCategory(ctx context.Context, id uuid.UUID) erro
 	return u.categoryRepo.DeleteCategory(ctx, id)
 }
 
-func (u *CategoryUseCase) GetCategories(ctx context.Context) ([]*domain_category.Category, error) {
-	return u.categoryRepo.GetCategories(ctx)
+func (u *CategoryUseCase) GetCategories(ctx context.Context) ([]dto.CategoryResponse, error) {
+	
+	categories, err := u.categoryRepo.GetCategories(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+	var categoriesDTO = make([]dto.CategoryResponse, 0, len(categories))		// capacity its equel to lenth of categories
+	
+	for _, cat := range categories {
+
+		category := dto.NewCategoryResponse(cat)
+
+		categoriesDTO = append(categoriesDTO, category)
+	}
+
+	return categoriesDTO, nil
+
+
+
 }
