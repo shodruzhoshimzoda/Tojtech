8ef05160de2ff9abb4ef554faf2505b4ba6c6f1e
shodruz <shodruzxoshimzoda@gmail.com>
2026-09-23 08:12:49 +0500

    Allow to all host connect to server

--- internal/delivery/http_server/routes.go
+++ internal/delivery/http_server/routes.go
@@ -24,8 +24,7 @@ func NewRoutes(
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"http://localhost:5173", "http://localhost:5500",
-			"http://127.0.0.1:5500"},
+		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
